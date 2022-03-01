f453597df6fa4f01b3cf87306560c9133ee59324
amandatorresapu <69279617+amandatorresapu@users.noreply.github.com>
2022-03-01 09:23:07 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// // alert("hello");
+// var count = "hello there";
+// var countElement = document.querySelector("#count");
+// console.log(countElement);
+
+// function add1() {
+//   countElement.innerText = "the count is " + count;
+//   console.log(count);
+// }
+
+// function subtract1() {
+//   countElement.innerText = "no " + count;
+//   console.log(count);
+// }
+
+
+var graph = "meow meow hershey";
+
+var changeElement = document.querySelector(#change);
+
+function changeTwo() {
+    changeElement.innerText = "boo boo " + graph; 
+};
+
+changeTwo()
+
